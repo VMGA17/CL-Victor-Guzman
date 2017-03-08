14e4d9636e024dbd25e60a60d8746d7df7fb71ae
vmga17 <victormanuelguzmanayala@gmail.com>
2017-03-08 16:51:36 -0600

    Examen

---
+++ metodos.js
@@ -0,0 +1,8 @@
+var CienciasyLetras = angular.module("CienciasyLetras", []);
+
+var dato = "12"
+var dato2 = "13"
+
+CienciasyLetras.controller ("Mensaje", function ($scope){
+		$scope.Suma = "12 + 13 = 25"
+})
